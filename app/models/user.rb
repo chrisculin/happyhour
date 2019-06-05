b2a4355017218288f2af02da4548e3d32d6cdcd8
Raghu Betina <raghu@firstdraft.com>
2019-06-05 16:19:08 +0000

    Direct association between users and comments

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   # Direct associations
 
+  has_many   :comments,
+             :dependent => :nullify
+
   has_many   :likes,
              :class_name => "Favorite",
              :dependent => :nullify
